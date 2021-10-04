9230f09129c73775ea9151349f6897979cd27c56
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2021-10-04 11:43:56 -0400

    funciones en memoria completadas

--- src/main/java/bo/edu/ucb/ingsoft/tasks/Task.java
+++ src/main/java/bo/edu/ucb/ingsoft/tasks/Task.java
@@ -1,12 +1,22 @@
 package bo.edu.ucb.ingsoft.tasks;
 
 public class Task {
+    private int idTask; 
     private String title;
     private String detail;
+    private String task_status;
+    private int status;
     
-    public Task(String title, String detail){
+    public Task(int idTask, String title, String detail, String task_status, int status){
+        this.idTask = idTask;
         this.title = title;
         this.detail = detail;
+        this.task_status = task_status;
+        this.status = status;
+    }
+
+    public int getIdTask() {
+        return idTask;
     }
 
     public String getTitle() {
@@ -17,12 +27,32 @@ public class Task {
         return detail;
     }
 
+    public String getTask_status() {
+        return task_status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setIdTask(int idTask) {
+        this.idTask = idTask;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
     
     public void setDetail(String detail) {
         this.detail = detail;
+    }
+
+    public void setTask_status(String task_status) {
+        this.task_status = task_status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
 }
--- src/main/java/bo/edu/ucb/ingsoft/tasks/Tasks.java
+++ src/main/java/bo/edu/ucb/ingsoft/tasks/Tasks.java
@@ -3,14 +3,20 @@ package bo.edu.ucb.ingsoft.tasks;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
 
 @RestController
 public class Tasks {
+    private int surrogateKey = 1;
 
     List<Task> taskList = new ArrayList();
     @GetMapping(path="/tasks", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,8 +26,75 @@ public class Tasks {
     
     @PostMapping(path="/tasks", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Task listAll(@RequestBody Task task){
+        task.setIdTask(surrogateKey);
         taskList.add(task);
+        surrogateKey++;
         return task;
+    }
+
+    @GetMapping(path="/tasks/{idTask}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Task getTask(@PathVariable int idTask){
+        Task task = findTaskById(idTask);
+        if(task == null) {
+            throw new RuntimeException("Task id not found - "+idTask);
+        }
+        return task;
+    }
+
+    public Task findTaskById(int idTb){
+        Task tb = null;
+        for (int i=0; i< taskList.size(); i++){
+            if(idTb == taskList.get(i).getIdTask()){
+                tb = taskList.get(i);
+                break;
+            }
+        }        
+        return tb;
+    }
+ /*   @PutMapping(path="/tasks/{idTask}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateTask(@PathVariable int idTask, @RequestBody Task t) {
+        Task task = findTaskById(idTask);
+        if(task != null) {
+            task.setTitle(t.getTitle());
+            task.setDetail(t.getDetail());
+        }        
+    }*/
+/*
+    @PutMapping(path="/tasks/{idTask}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateStateTask(@PathVariable int idTask, @RequestParam(value = "task_tatus", defaultValue = "PENDING") String task_status) {
+        System.out.println("estado que llega"+ task_status);
+        /*Task task = findTaskById(idTask);
+        if(task != null) {
+            task.setTitle(t.getTitle());
+            task.setDetail(t.getDetail());
+        }        
+    }*/
+
+    @PutMapping(path="/tasks/{idTask}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String updateTask(@RequestParam(required = false) String task_status, @PathVariable int idTask, @RequestBody Task t) { 
+        String msg = null;
+        Task task = findTaskById(idTask);
+        if(task != null) {
+            if (task_status == null) {
+                task.setTitle(t.getTitle());
+                task.setDetail(t.getDetail());
+                msg = "task modified";
+            }else{
+                task.setTask_status(task_status);
+                msg = "task status modified";
+            }
+           
+        }
+        return msg;
+    }
+
+    @DeleteMapping(path="/tasks/{idTask}")
+    public void DeleteTask(@PathVariable int idTask) {
+        Task task = findTaskById(idTask);
+        if(task != null) {
+            task.setStatus(0);
+        }      
     }
     
 }
